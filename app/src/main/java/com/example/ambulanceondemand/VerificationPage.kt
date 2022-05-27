8965176851751e35a60189d232984e8cc9523b5c
luthfirrdz <luthfirluthfir@gmail.com>
2022-05-28 01:10:46 +0700

    80% verification page UI

---
+++ app/src/main/java/com/example/ambulanceondemand/VerificationPage.kt
@@ -0,0 +1,11 @@
+package com.example.ambulanceondemand
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class VerificationPage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_verification_page)
+    }
+}
