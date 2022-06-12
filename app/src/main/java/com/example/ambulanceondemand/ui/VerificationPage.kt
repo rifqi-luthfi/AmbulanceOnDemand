62961dda93272440819689ad997ef5a6151c7070
rifqi-luthfi <rluthfi711@gmail.com>
2022-06-12 20:54:05 +0700

    TFLite 100%

--- app/src/main/java/com/example/ambulanceondemand/ui/VerificationPage.kt
+++ app/src/main/java/com/example/ambulanceondemand/ui/VerificationPage.kt
@@ -2,16 +2,26 @@ package com.example.ambulanceondemand.ui
 
 import android.content.Intent
 import android.graphics.BitmapFactory
+import android.graphics.drawable.BitmapDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
+import com.example.ambulanceondemand.R
 import com.example.ambulanceondemand.databinding.ActivityVerificationPageBinding
 import com.example.ambulanceondemand.util.rotateBitmap
+import com.google.tflite.catvsdog.tflite.Classifier
 import java.io.File
 
 class VerificationPage : AppCompatActivity() {
+    private val mInputSize = 350
+    private val mModelPath = "converted_model_minggu.tflite"
+    private val mLabelPath = "labels.txt"
+    private lateinit var classifier: Classifier
 
     companion object {
         const val CAMERA_X_RESULT = 200
@@ -25,7 +35,20 @@ class VerificationPage : AppCompatActivity() {
         binding = ActivityVerificationPageBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        initClassifier()
+        initViews()
+
         binding.ivUpload.setOnClickListener { startCameraX() }
+    }
+
+    private fun initClassifier() {
+        classifier = Classifier(assets, mModelPath, mLabelPath, mInputSize)
+    }
+
+    private fun initViews() {
+        findViewById<ImageView>(R.id.ivUpload)
+
+//        runOnUiThread { Toast.makeText(this, result.get(0).title, Toast.LENGTH_SHORT).show() }
     }
 
     private var getFile: File? = null
@@ -41,14 +64,19 @@ class VerificationPage : AppCompatActivity() {
                 BitmapFactory.decodeFile(myFile.path),
                 isBackCamera
             )
-            binding.apply {
-                Glide.with(this@VerificationPage)
-                    .load(result)
-                    .transition(DrawableTransitionOptions.withCrossFade())
-                    .centerCrop()
-                    .into(ivUpload)
 
+            binding.ivUpload.setImageBitmap(result)
+
+            val bitmap = ((binding.ivUpload).drawable as BitmapDrawable).bitmap
+
+            val predict = classifier.recognizeImage(bitmap)
+
+            if (predict.get(0).title == "No_accident"){
+                binding.tvStatusAccident.text = "Tidak Tergolong Kecelakaan"
+            }else{
+                binding.tvStatusAccident.text = "Tergolong Kecelakaan"
             }
+
         }
     }
 
