9e267852c7c454e44fece5369f7ddb12e28dee37
luthfirrdz <luthfirluthfir@gmail.com>
2022-06-12 19:13:22 +0700

    manifest : change maps api key to AIzaSyB2gcy6TtMyK59A9PM2RBQMyDf20tmlygw camera activity : implementation camera x verification page : set ivUpload to preview photo from camera x
    add file Utils.kt to util folder for camera x settings

--- app/src/main/java/com/example/ambulanceondemand/ui/VerificationPage.kt
+++ app/src/main/java/com/example/ambulanceondemand/ui/VerificationPage.kt
@@ -1,12 +1,59 @@
 package com.example.ambulanceondemand.ui
 
+import android.content.Intent
+import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.ambulanceondemand.R
+import androidx.activity.result.contract.ActivityResultContracts
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
+import com.example.ambulanceondemand.databinding.ActivityVerificationPageBinding
+import com.example.ambulanceondemand.util.rotateBitmap
+import java.io.File
 
 class VerificationPage : AppCompatActivity() {
+
+    companion object {
+        const val CAMERA_X_RESULT = 200
+    }
+
+    private lateinit var binding: ActivityVerificationPageBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_verification_page)
+
+        binding = ActivityVerificationPageBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.ivUpload.setOnClickListener { startCameraX() }
+    }
+
+    private var getFile: File? = null
+    private val launcherIntentCameraX = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) {
+        if (it.resultCode == CAMERA_X_RESULT) {
+            val myFile = it.data?.getSerializableExtra("picture") as File
+            val isBackCamera = it.data?.getBooleanExtra("isBackCamera", true) as Boolean
+
+            getFile = myFile
+            val result = rotateBitmap(
+                BitmapFactory.decodeFile(myFile.path),
+                isBackCamera
+            )
+            binding.apply {
+                Glide.with(this@VerificationPage)
+                    .load(result)
+                    .transition(DrawableTransitionOptions.withCrossFade())
+                    .centerCrop()
+                    .into(ivUpload)
+
+            }
+        }
+    }
+
+    private fun startCameraX() {
+            val intent = Intent(this, CameraActivity::class.java)
+            launcherIntentCameraX.launch(intent)
     }
 }
