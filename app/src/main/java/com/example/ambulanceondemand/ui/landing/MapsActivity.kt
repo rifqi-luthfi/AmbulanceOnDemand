799de3f90ec562d13623008571dd66d044e83d16
luthfirrdz <luthfirluthfir@gmail.com>
2022-06-12 14:46:23 +0700

    maps activity : add current location user; layout : activity_maps textview to textcolor: black; build gradle: add dependencies gmaps; manifest: add permission fine & coarse location;

--- app/src/main/java/com/example/ambulanceondemand/ui/landing/MapsActivity.kt
+++ app/src/main/java/com/example/ambulanceondemand/ui/landing/MapsActivity.kt
@@ -1,7 +1,15 @@
 package com.example.ambulanceondemand.ui.landing
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Geocoder
+import android.location.Location
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
 import com.example.ambulanceondemand.R
 
 import com.google.android.gms.maps.CameraUpdateFactory
@@ -11,11 +19,15 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import com.example.ambulanceondemand.databinding.ActivityMapsBinding
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import java.util.*
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
     private lateinit var binding: ActivityMapsBinding
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,27 +35,112 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         binding = ActivityMapsBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        getMyLastLocation()
+
     }
+
+    private val requestPermissionLauncher =
+        registerForActivityResult(
+            ActivityResultContracts.RequestMultiplePermissions()
+        ) { permissions ->
+            when {
+                permissions[Manifest.permission.ACCESS_FINE_LOCATION] ?: false -> {
+                    // Precise location access granted.
+                    getMyLastLocation()
+                }
+                permissions[Manifest.permission.ACCESS_COARSE_LOCATION] ?: false -> {
+                    // Only approximate location access granted.
+                    getMyLastLocation()
+                }
+                else -> {
+                    // No location access granted.
+                }
+            }
+        }
+    private fun checkPermission(permission: String): Boolean {
+        return ContextCompat.checkSelfPermission(
+            this,
+            permission
+        ) == PackageManager.PERMISSION_GRANTED
+    }
+    private fun getMyLastLocation() {
+        if     (checkPermission(Manifest.permission.ACCESS_FINE_LOCATION) &&
+            checkPermission(Manifest.permission.ACCESS_COARSE_LOCATION)
+        ){
+            fusedLocationClient.lastLocation.addOnSuccessListener { location: Location? ->
+                if (location != null) {
+                    showStartMarker(location)
+                } else {
+                    Toast.makeText(
+                        this@MapsActivity,
+                        "Location is not found. Try Again",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+        } else {
+            requestPermissionLauncher.launch(
+                arrayOf(
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                )
+            )
+        }
+    }
+    private fun showStartMarker(location: Location) {
+        val startLocation = LatLng(location.latitude, location.longitude)
+        mMap.addMarker(
+            MarkerOptions()
+                .position(startLocation)
+                .title(getString(R.string.start_point))
+        )
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(startLocation, 17f))
+        Log.d("Debug:" ,"Your Location:"+ location.longitude)
+        binding.actvPickUpLocation.text = getCityName(location.latitude,location.longitude)
+    }
+
+//    private fun getCityName(location: Location):String{
+//        var cityName:String = ""
+//        var countryName = ""
+//        var geoCoder = Geocoder(this, Locale.getDefault())
+//        var Adress = geoCoder.getFromLocation(location.latitude,location.longitude,3)
+//
+//        cityName = Adress.get(0).locality
+//        countryName = Adress.get(0).countryName
+//        Log.d("Lokasi gw","Your City: " + cityName + " ; your Country " + countryName)
+//        return cityName
+//    }
+
+    private fun getCityName(lat: Double,long: Double):String{
+        var geoCoder = Geocoder(this, Locale.getDefault())
+        var Adress = geoCoder.getFromLocation(lat,long,5)
+
+        val street = Adress.get(0).thoroughfare
+        val numStreet = Adress.get(0).subThoroughfare
+        val kecamatan = Adress.get(0).locality
+        val kota = Adress.get(0).adminArea
+        val countryName = Adress.get(0).countryName
+        val cityName = "$street $numStreet, $kecamatan, $kota"
+        Log.d("Lokasi sekarang","Your street: " + street + " ; Your City: " + cityName + " ; your Country " + countryName)
+        return cityName
+
+        /**
+         * sublocality = kelurahan
+         * locality = kecamatan
+         * countryName = negara
+         * thoroughfare = nama jalan
+         * subthoroughfare = nomor rumah
+         */
+    }
+
 }
